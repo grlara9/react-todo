62c68cb5645096cac05d1bd7f93f7af53f001931
Guillermo Lara <grlara9@gmail.com>
2020-01-27 21:17:53 -0800

    edit header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ import { Jumbotron, Container } from 'react-bootstrap'
 export default function Header() {
     return (
         <div>
-            <Jumbotron fluid style={getStyle}> 
+            <Jumbotron style={getStyle}> 
                  <Container >
                 <h1>Todo List</h1>
                 <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
@@ -15,15 +15,14 @@ export default function Header() {
     )
 }
 const getStyle = {
-    background: '#333',
-    color: 'white',
+    
     textAlign: 'center',
     padding: '5px',
     fontSize: '28px'
 }
 
 const linkStyle ={
-    color: 'white',
+    color: 'black',
     fontSize: '24px',
     textDecoration: 'none',
 }
